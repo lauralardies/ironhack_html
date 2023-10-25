15196dd9c791f301cdc201f34368d92b204b01f3
lauralardies <lrodrlar@myuax.com>
2023-10-25 11:53:28 +0200

    punto coma

--- Week 3/Day 8/fizzbuzz.js
+++ Week 3/Day 8/fizzbuzz.js
@@ -1,6 +1,6 @@
 for (let i = 50; i >= 1; i--){
     if (i % 2 !== 0 && i-1 % 10 !== 0) {
-      console.log("Monkey!")
+      console.log("Monkey!");
     } else if (i % 15 === 0 || i % 10 === 0) {
       console.log("Donkey!");
     } else if (i % 5 === 0) {
